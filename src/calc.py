5c7bffc96f10a45bbf3d35093c689fcaebed4962
boris-isac <borisisac@gmail.com>
2025-09-25 12:59:05 +0100

    [ADD]tests for 2 methods (minus and mult)

--- src/calc.py
+++ src/calc.py
@@ -1,10 +1,22 @@
-class Calc:
+def _validated_data(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Operators must be numbers and not str")
 
+class Calc:
     def add(self, a, b):
-        return a+b
+        _validated_data(a,b)
+        return a + b
+
+    def minus(self, a, b):
+        _validated_data(a, b)
+        return a - b
 
     def div(self, a, b):
+        _validated_data(a, b)
         if b != 0:
-            return a/b
+            return a / b
         return "ZeroDivisionError"
 
+    def mult(self, a, b):
+        _validated_data(a, b)
+        return a * b
--- tests/test_calc_DDT.py
+++ tests/test_calc_DDT.py
@@ -1,32 +1,80 @@
 import pytest
 
+
 def test_first():
     name = "Boris"
     assert name == 'Boris', "Not upper"
 
+
 @pytest.mark.parametrize("a,b,expected", [
-    (0,0,0),
-    (0,1,1),
-    (1,0,1),
-    (-1,4,3),
-    (4,-1,3),
-    (-4,-2,-6),
-    (5.5,4.4,9.9),
-    (5.5,4.5,10.0),
+    ("a", 0, TypeError),
+    (0, "a", TypeError),
+    (0, 0, 0),
+    (0, 1, 1),
+    (1, 0, 1),
+    (-1, 4, 3),
+    (4, -1, 3),
+    (-4, -2, -6),
+    (5.5, 4.4, 9.9),
+    (5.5, 4.5, 10.0),
 ])
-def test_add(calc,a,b,expected):
-    assert calc.add(a,b) == expected
+def test_add(calc, a, b, expected):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            calc.add(a, b)
+    else:
+        assert calc.add(a, b) == expected
 
 
 @pytest.mark.parametrize("a,b,expected", [
-    (10,2,5),
-    (2,10,0.2),
-    (1,3,1/3),
-    (0,1,0),
-    (1,0,"ZeroDivisionError")
+    (10, 2, 5),
+    (2, 10, 0.2),
+    (1, 3, 1 / 3),
+    (0, 1, 0),
+    (1, 0, "ZeroDivisionError"),
+    ("a", 0, TypeError),
+    (0, "a", TypeError),
 ])
-def test_div(calc,a,b,expected):
+def test_div(calc, a, b, expected):
     if isinstance(expected, type) and issubclass(expected, Exception):
         with pytest.raises(expected):
             calc.div(a, b)
-    assert calc.div(a,b) == pytest.approx(expected)
+    else:
+        assert calc.div(a, b) == pytest.approx(expected)
+
+
+@pytest.mark.parametrize("a,b,expected", [
+    (10, 2, 20),
+    (0.2, 10, 2),
+    (-1, 3, -3),
+    (1, -3, -3),
+    (0, 1, 0),
+    (1, 0, 0),
+    ("a", 0, TypeError),
+    (0, "a", TypeError),
+
+])
+def test_mult(calc, a, b, expected):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            calc.div(a, b)
+    else:
+        assert calc.mult(a, b) == expected
+
+@pytest.mark.parametrize("a,b,expected", [
+    (10, 2, 8),
+    (0.2, 10, -9.8),
+    (-1, 3, -4),
+    (1, -3, 4),
+    (0, 1, -1),
+    (1, 0, 1),
+    ("a", 0, TypeError),
+    (0, "a", TypeError),
+
+])
+def test_minus(calc, a, b, expected):
+    if isinstance(expected, type) and issubclass(expected, Exception):
+        with pytest.raises(expected):
+            calc.minus(a, b)
+    else:
+        assert calc.minus(a, b) == expected
