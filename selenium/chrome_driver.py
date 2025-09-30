96ef4057ff8b8d8aa0e19bb37c6d0a8839ec3595
boris-isac <borisisac@gmail.com>
2025-09-30 14:41:57 +0100

    [ADD]find elements by xpath selenium

--- selenium/chrome_driver.py
+++ selenium/chrome_driver.py
@@ -16,6 +16,7 @@ URL_TEST5 = "https://webtech87.pt/"
 
 service = Service(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
+'''
 
 
 driver.get(URL_TEST)
@@ -60,8 +61,29 @@ for obj in a:
     obj.click()
     sleep(2)
 
+'''
+
+driver.get(URL_TEST4)
+button_coockies_accept = driver.find_element('xpath', "//button[@id='onetrust-accept-btn-handler']")
+print(button_coockies_accept)
+button_coockies_accept.click()
+sleep(3)
+login_link = driver.find_element('xpath', "//a[@data-cy='myolx-link']").click()
+sleep(2)
+input_fields = driver.find_elements("tag name", "input")
+print(len(input_fields))
+input_fields[0].send_keys("Pupsik")
+input_fields[1].send_keys("Pupsik")
 
 
+username_field = driver.find_element("xpath", "//input[@id='username']")
+password_field = driver.find_element("xpath", "//input[@id='password']")
+#username_field.send_keys("borisisac@gmail.com")
+#password_field.send_keys("spawnkid1")
+sleep(3)
+
+
+#<a href="http://www.olx.pt/account/?ref[0][params][url]=http%3A%2F%2Fwww.olx.pt%2F&amp;ref[0][action]=redirector&amp;ref[0][method]=index" data-cy="myolx-link" data-testid="myolx-link" class="css-12l1k7f"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" class="css-12my6f1"><path fill="currentColor" fill-rule="evenodd" d="M12 12c4.963 0 9 4.038 9 9l-1 1H4l-1-1c0-4.962 4.037-9 9-9zm0 2c-3.52 0-6.442 2.613-6.929 6H18.93c-.487-3.387-3.409-6-6.93-6zm0-12c2.481 0 4.5 2.019 4.5 4.5 0 2.482-2.019 4.5-4.5 4.5a4.505 4.505 0 0 1-4.5-4.5C7.5 4.019 9.519 2 12 2zm0 2a2.503 2.503 0 0 0-2.5 2.5C9.5 7.878 10.621 9 12 9s2.5-1.122 2.5-2.5S13.379 4 12 4z"></path></svg>A tua conta<div class="css-oedrow"></div></a>
 
 
 
